1ff1c5232774ec7d3f76da48a41fadc9b126b58c
zoowar <zoowar@qq.com>
2019-03-09 12:21:21 +0800

    Signed-off-by: zoowar <zoowar@qq.com>

---
+++ html/tool.vue
@@ -0,0 +1,12 @@
+<template>
+	<button class="add">添加</button>
+</template>
+<script>
+	exports default{
+		name:"tool",	
+	}
+</script>
+
+<style>
+	.add{width:10rem;height:2.5rem;background-color:#f00;color:#fff;font-size:18px;}
+</style>
---
+++ js/cart.js
@@ -0,0 +1,28 @@
+new Vue({
+	el:".container",
+	data:{
+		productList:[]
+	},
+	filters:{
+		
+	},
+	mounted:function(){
+		this.$nextTick(
+			function(){
+				this.cartView();
+			}
+		);
+	},
+	methods:{
+		cartView:function(){		
+			console.log(222);
+			var _this=this;
+			this.$http.get("data/cartData.json",{"id":123}).then(function(res){	
+				console.log(res);
+			   _this.productList=res.data.result.list;
+			});
+		}
+	}
+	
+});
+Vue.prototype.$http=axios
